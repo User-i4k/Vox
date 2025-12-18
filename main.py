c3c9a9739ec4bc315b5153b88743c80c20f7d222
User-i4k <User-i4k@users.noreply.github.com>
2025-12-19 01:44:01 +0300

    Vox

--- main.py
+++ main.py
@@ -1,26 +1,28 @@
-m = input("Sen: ")
-mesaj = m.lower()
-
-kelimeler = mesaj.split()
-
 selam = ["selam", "merhaba"]
+hal = ["nasılsın", "naber", "ne", "haber"]
 
-hal = ["nasılsın", "naber", "ne haber"]
+while True:
+    m = input("Sen: ")
+    mesaj = m.lower()
+    kelimeler = mesaj.split()
 
-selam_var = False
-hal_var = False
+    selam_var = False
+    hal_var = False
 
-for kelime in kelimeler:
-    if kelime in selam:
-        selam_var = True
-    if kelime in hal:
-        hal_var = True
+    for kelime in kelimeler:
+        if kelime in selam:
+            selam_var = True
+        if kelime in hal:
+            hal_var = True
 
-if selam_var and hal_var:
-    print("Vox: Selam! İyiyim, teşekkürler. Sen nasılsın?")
-elif selam_var:
-    print("Vox: Selam! Nasılsın?")
-elif hal_var:
-    print("Vox: İyiyim, teşekkürler. Sen nasılsın?")
-else:
-    print("Vox: Anlamadım, lütfen tekrar eder misin?")
+    if selam_var and hal_var:
+        print("Vox: Selam! İyiyim, teşekkürler. Sen nasılsın?")
+    elif selam_var:
+        print("Vox: Selam! Nasılsın?")
+    elif hal_var:
+        print("Vox: İyiyim, teşekkürler. Sen nasılsın?")
+    elif mesaj in ["çık", "exit", "quit"]:
+        print("Vox: Görüşürüz 👋")
+        break
+    else:
+        print("Vox: Anlamadım, lütfen tekrar eder misin?")
